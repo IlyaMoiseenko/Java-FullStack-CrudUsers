1b81a814582d66588fc3e3d7e30021c7260eb8da
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-14 21:13:08 +0300

    Delete UserRepository

--- backend/src/main/java/com/markalfox/web/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.markalfox.web.repository;
-
-import com.markalfox.web.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
