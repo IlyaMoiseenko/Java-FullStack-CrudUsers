42adebf5557da5b1db9ea6091c14a921794b9e86
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-14 22:08:29 +0300

    Create home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./layout/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+        <NavBar />
+        <Home />
     </div>
   );
 }
