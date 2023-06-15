d2745bb5515bf1914b342cf8e51b7dee930e30cf
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-15 21:13:25 +0300

    implemented the ability to add a user

--- frontend/src/layout/NavBar.jsx
+++ frontend/src/layout/NavBar.jsx
@@ -6,7 +6,13 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">CrudUser</a>
+                    <Link
+                        className="navbar-brand"
+                        to="/"
+                    >
+                        CrudUser
+                    </Link>
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
--- frontend/src/users/AddUser.jsx
+++ frontend/src/users/AddUser.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
+import {Link, useNavigate} from "react-router-dom";
 
 const AddUser = () => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name: "",
+        userName: "",
+        email: ""
+    })
+
+    const onInputChange = (event) => {
+        setUser({...user, [event.target.name]: event.target.value})
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+
+        await axios.post("http://localhost:8080/user", user)
+
+        navigate("/")
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -9,23 +30,52 @@ const AddUser = () => {
                         Register User
                     </h2>
 
-                    <div className="mb-3">
-                        <label htmlFor="Name" className="form-label">Name</label>
-                        <input type={"text"} className="form-control" placeholder="Enter name of user" name="name"/>
-                    </div>
+                    <form onSubmit={(event) => onSubmit(event)}>
+                        <div className="mb-3">
+                            <label htmlFor="Name" className="form-label">Name</label>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter name of user"
+                                name="name"
+                                value={user.name}
+                                onChange={(event) => onInputChange(event)}
+                            />
+                        </div>
 
-                    <div className="mb-3">
-                        <label htmlFor="Username" className="form-label">Username</label>
-                        <input type={"text"} className="form-control" placeholder="Enter user name" name="userName"/>
-                    </div>
+                        <div className="mb-3">
+                            <label htmlFor="Username" className="form-label">Username</label>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter user name"
+                                name="userName"
+                                value={user.username}
+                                onChange={(event) => onInputChange(event)}
+                            />
+                        </div>
 
-                    <div className="mb-3">
-                        <label htmlFor="Email" className="form-label">Email</label>
-                        <input type={"text"} className="form-control" placeholder="Enter email of user" name="email"/>
-                    </div>
+                        <div className="mb-3">
+                            <label htmlFor="Email" className="form-label">Email</label>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter email of user"
+                                name="email"
+                                value={user.email}
+                                onChange={(event) => onInputChange(event)}
+                            />
+                        </div>
 
-                    <button type="submit" className="btn btn-outline-primary">Submit</button>
-                    <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+                        <button type="submit" className="btn btn-outline-primary">Submit</button>
+                        <Link
+                            type="submit"
+                            className="btn btn-outline-danger mx-2"
+                            to="/"
+                        >
+                            Cancel
+                        </Link>
+                    </form>
                 </div>
             </div>
         </div>
