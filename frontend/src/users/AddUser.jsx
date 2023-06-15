85f7b5f33ebef41600f0bcf0508f56afb83e199d
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-15 20:47:45 +0300

    Create register user form

--- frontend/src/users/AddUser.jsx
+++ frontend/src/users/AddUser.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 const AddUser = () => {
     return (
-        <div>
-            <h1>Add User</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                    <h2 className="text-center m-4">
+                        Register User
+                    </h2>
+
+                    <div className="mb-3">
+                        <label htmlFor="Name" className="form-label">Name</label>
+                        <input type={"text"} className="form-control" placeholder="Enter name of user" name="name"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="Username" className="form-label">Username</label>
+                        <input type={"text"} className="form-control" placeholder="Enter user name" name="userName"/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="Email" className="form-label">Email</label>
+                        <input type={"text"} className="form-control" placeholder="Enter email of user" name="email"/>
+                    </div>
+
+                    <button type="submit" className="btn btn-outline-primary">Submit</button>
+                    <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+                </div>
+            </div>
         </div>
     );
 };
