28b257a0713b1a645149c35ed58499841b8b4f7c
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-16 07:36:01 +0300

    Add custom exceptions

--- backend/src/main/java/com/markalfox/web/controller/HumanController.java
+++ backend/src/main/java/com/markalfox/web/controller/HumanController.java
@@ -1,16 +1,15 @@
 package com.markalfox.web.controller;
 
+import com.markalfox.web.exception.UserNotFoundException;
 import com.markalfox.web.model.Human;
 import com.markalfox.web.repository.HumanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class HumanController {
     private final HumanRepository humanRepository;
 
@@ -27,5 +26,10 @@ public class HumanController {
     @GetMapping("/users")
     public List<Human> gteAllUser() {
         return humanRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public Human getUserById(@PathVariable long id) {
+        return humanRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
