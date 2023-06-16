b9149ed9609636c3f76f78d2a918ed9f6cdac72c
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-16 18:46:12 +0300

    Delete user on front-end

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 const Home = () => {
     const [users, setUsers] = useState([])
+    const {id} = useParams()
 
     useEffect(() => {
         loadUsers()
@@ -12,6 +13,11 @@ const Home = () => {
     const loadUsers = async () => {
         const result = await axios.get("http://localhost:8080/users")
         setUsers(result.data)
+    }
+
+    const deleteUser = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers()
     }
 
     return (
@@ -50,7 +56,7 @@ const Home = () => {
                                         Edit
                                     </Link>
 
-                                    <button className="btn btn-danger mx-2">Delete</button>
+                                    <button onClick={() => deleteUser(user.id)} className="btn btn-danger mx-2">Delete</button>
                                 </td>
                             </tr>
                         ))
