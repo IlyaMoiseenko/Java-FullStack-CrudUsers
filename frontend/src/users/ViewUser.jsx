75ce7a059e5efb50ea01b7e02993ee8ec2bbdcce
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-18 14:03:46 +0300

    View user on front-end

--- frontend/src/users/ViewUser.jsx
+++ frontend/src/users/ViewUser.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 
 const ViewUser = () => {
+    const {id} = useParams()
+
+    const [user, setUser] = useState({
+        name: "",
+        userName: "",
+        email: ""
+    })
+
+    useEffect(() => {
+        loadUser()
+    }, [])
+
+    const loadUser = async () => {
+        const result = await axios.get(`http://localhost:8080/user/${id}`)
+
+        setUser(result.data)
+    }
+
     return (
         <div>
-            <h1>View User</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                        <h2 className="text-center m-4">
+                            User details
+                        </h2>
+
+                        <div className="card">
+                            <div className="card-header">
+                                Details of user id: {user.id}
+
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">
+                                        <b>Name: {user.name}</b>
+                                    </li>
+
+                                    <li className="list-group-item">
+                                        <b>User name: {user.userName}</b>
+                                    </li>
+
+                                    <li className="list-group-item">
+                                        <b>Email: {user.email}</b>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <Link className="btn btn-primary my-2" to={"/"}>Back to Home</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
